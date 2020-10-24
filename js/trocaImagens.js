2a71d1a86c8f82e743e4864f3e5e3b97422da926
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2020-10-24 17:20:32 -0300

    Arrumei a largura do menu mobile para se comportar em diferentes telas

---
+++ js/trocaImagens.js
@@ -0,0 +1,21 @@
+const body2 = document.getElementsByTagName('body')[0]
+const proximo = document.getElementsByTagName('button')[0]
+const anterior = document.getElementsByTagName('button')[1]
+
+const foto = document.getElementsByTagName('img')[0]
+
+let listaDeFotos = ['photo1.jpg', 'photo2.jpg']
+let i = 0
+proximo.addEventListener('click', () => {
+    foto.setAttribute('src', `images/${listaDeFotos[i++]}`)
+    if(i > listaDeFotos.length-1){
+        i = 0
+    }
+})
+
+anterior.addEventListener('click', () => {
+    foto.setAttribute('src', `images/${listaDeFotos[i--]}`)
+    if(i < 0){
+        i = listaDeFotos.length-1
+    }
+})
