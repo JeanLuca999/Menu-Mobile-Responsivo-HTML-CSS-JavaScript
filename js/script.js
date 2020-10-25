7a17cf8502bbca49b3ab211c10a2a41c75681862
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2020-10-25 16:58:40 -0300

    Juntei os arquivos js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//funções do menu mobile
 const botaoMenuMobile = document.getElementsByClassName('botao-menu-mobile')[0]
 const menu = document.getElementsByClassName('menu')[0]
 const body = document.getElementsByTagName('body')[0]
@@ -15,5 +16,27 @@ botaoMenuMobile.addEventListener('click', () => {
         botaoMenuMobile.style.backgroundColor = 'rgb(83, 125, 163)'
         body.style.overflow = 'auto'
         clicouNoMenu = false
+    }
+})
+
+//funções para trocar imagem
+const proximo = document.getElementsByTagName('button')[0]
+const anterior = document.getElementsByTagName('button')[1]
+
+const foto = document.getElementsByTagName('img')[0]
+
+let listaDeFotos = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg', 'photo4.jpg', 'photo5.jpg', 'photo6.jpg']
+let i = 0
+proximo.addEventListener('click', () => {
+    foto.setAttribute('src', `images/${listaDeFotos[i++]}`)
+    if(i > listaDeFotos.length-1){
+        i = 0
+    }
+})
+
+anterior.addEventListener('click', () => {
+    foto.setAttribute('src', `images/${listaDeFotos[i--]}`)
+    if(i < 0){
+        i = listaDeFotos.length-1
     }
 })
